ca201581800c164102b116cc72caefd4693d763f
Hanyu Yang <42820116+rhit-yangh6@users.noreply.github.com>
2022-11-25 16:50:49 -0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const ICONFONT_URL = '//at.alicdn.com/t/font_2638499_eb47axm7kct.js'
-export const API_URL = 'http://137.184.226.196:8080/64collection'
+export const API_URL = 'https://www.64collection.com/64collection'
